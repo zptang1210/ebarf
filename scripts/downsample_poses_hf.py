1d278ba20d49efe451de24df590e2d5664b3d395
Zhipeng <zhipengtang@umass.edu>
2024-03-30 05:48:36 +0000

    update downsample_poses_hf.py

--- scripts/downsample_poses_hf.py
+++ scripts/downsample_poses_hf.py
@@ -1,23 +1,27 @@
 import pickle
 import torch
 
-poses_hf_load_path = 'output/poses_hf_cache/noised_poses_hf_1e-2.pickle'
-poses_hf_save_path = 'output/poses_hf_cache/ds_poses_hf_30hz.pickle'
+freq = 5 # Hz
+threshold = 1e9 / freq
+# e.g. threshold = 33333333.3 #ns = 1/30s = 30 Hz
+# threshold = 2e+8 # = 1/5s = 5Hz
+
+poses_hf_load_path = '../output/poses_hf_cache/gt_poses_hf.pickle'
+poses_hf_save_path = f'../output/poses_hf_cache/ds_poses_hf_{freq}hz.pickle'
 
 if __name__ == '__main__':
     with open(poses_hf_load_path, 'rb') as fin:
         poses_hf_dict = pickle.load(fin)
 
-    tss_poses_hf_ns = poses_hf_dict['tss_poses_hf_ns'].detach().cpu()
     # * raw was chosen because we want to downsample fromn the gt poses_hf
     # * if we want to downsample the altered (e.g. noised) poses_hf, we change it to poses_hf
+    tss_poses_hf_ns = poses_hf_dict['raw_tss_poses_hf_ns'].detach().cpu()
     poses_hf = poses_hf_dict['raw_poses_hf'].detach().cpu() 
 
     tss_diff = torch.diff(tss_poses_hf_ns)
     # print('diff of tss_poses_hf_ns:\n', tss_diff)
 
     acc = 0
-    threshold = 33333333.3 #ns =  1/30s 
     ds_idxs = [0] # always insert the first point
     for idx, diff in enumerate(tss_diff):
         if acc > threshold:
@@ -45,7 +49,7 @@ if __name__ == '__main__':
         'tss_poses_hf_ns': ds_tss_poses_hf_ns,
         'raw_poses_hf': poses_hf,
         'raw_tss_poses_hf_ns': tss_poses_hf_ns,
-        'comment': 'downsample the raw poses_hf to 30Hz.'
+        'comment': f'downsample the raw poses_hf to {freq}Hz.'
     }
 
     with open(poses_hf_save_path, 'wb') as fout:
