701fbe4c9a0edf1977c217f41a0901ce7b9ffc80
Zhipeng <zhipengtang@umass.edu>
2024-04-09 00:14:57 +0000

    update configs

---
+++ sbatch_run_ebarf_aug_patience_100_mocapDesk2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -c 16  # Number of Cores per Task
+#SBATCH --mem=65536  # Requested Memory
+#SBATCH -p gypsum-2080ti  # Partition
+#SBATCH -G 1  # Number of GPUs
+#SBATCH -t 24:00:00  # Job time limit
+#SBATCH -o slurm-aug-%j.out  # %j = job ID
+module load cuda/11.4.0
+module load cudnn/cuda11-8.4.1.50
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate enerf_cloned
+python main_barf.py --config ./configs/mocapDesk2/mocapDesk2_ebarf_start_from_5hz_aug_patience_100.txt
---
+++ sbatch_run_enerf_override_poseshf_mocapDesk2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -c 16  # Number of Cores per Task
+#SBATCH --mem=65536  # Requested Memory
+#SBATCH -p gypsum-2080ti  # Partition
+#SBATCH -G 1  # Number of GPUs
+#SBATCH -t 168:00:00  # Job time limit
+#SBATCH -o slurm-enerf-%j.out  # %j = job ID
+module load cuda/11.4.0
+module load cudnn/cuda11-8.4.1.50
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate ebarf
+python main_nerf_with_poses_hf_override.py --config ./configs/mocapDesk2/mocapDesk2_enerf_override_poses_hf.txt --precompute_evs_poses 0
+
